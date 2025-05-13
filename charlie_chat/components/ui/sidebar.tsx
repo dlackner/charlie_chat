ad108277b6a314c1d47a841fe8a6042a088b8daf
david <dplackner@gmail.com>
2025-05-12 22:19:13 -0700

    toggle selection bug fix

--- charlie_chat/components/ui/sidebar.tsx
+++ charlie_chat/components/ui/sidebar.tsx
@@ -841,8 +841,8 @@ export const Sidebar = ({
                 </button>
                 <button
                   type="button"
-                  onClick={() => setMlsActive("Yes")} // Set to "Any"
-                  className={`relative inline-flex items-center px-4 py-2 rounded-l-md border border-gray-300 text-sm font-medium
+                  onClick={() => setMlsActive("true")} // Set to "Yes"
+                  className={`relative inline-flex items-center px-4 py-2 -ml-px border border-gray-300 text-sm font-medium 
                               ${mlsActive === "true" ? 'bg-orange-500 text-white z-10 ring-1 ring-orange-500 border-orange-500' : 'bg-white text-gray-700 hover:bg-gray-50'} 
                               focus:z-10 focus:outline-none focus:ring-1 focus:ring-orange-500 focus:border-orange-500`}
                 >
@@ -859,6 +859,7 @@ export const Sidebar = ({
                 </button>
               </div>
             </div>
+
 
             <div>
               <label className="block text-sm font-medium text-gray-700 mb-1">Last Sale Arms Length?</label>
